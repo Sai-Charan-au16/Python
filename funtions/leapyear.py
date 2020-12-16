dfb20e80a3d5108d5bbe5e3c86ebf0d3196d8a24
Sai charan <panjalasaicharan9@gmail.com>
2020-12-16 11:40:24 +0530

    python

---
+++ funtions/leapyear.py
@@ -0,0 +1,16 @@
+#leap year
+
+
+def is_leap(year):
+    leap = False
+    
+    if year % 400 == 0 :
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True# Write your logic here
+    
+    return leap
+
+year = int(input())
