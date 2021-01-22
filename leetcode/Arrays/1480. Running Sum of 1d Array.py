1de1967f1efdda04722670f2b66f6f14cf8f4e59
Sai charan <panjalasaicharan9@gmail.com>
2021-01-22 11:34:39 +0530

    leetcode

---
+++ leetcode/Arrays/1480. Running Sum of 1d Array.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/running-sum-of-1d-array/
+
+class Solution(object):
+    def runningSum(self, nums):
+        
+        a = 0
+        lst = []
+        for i in nums:
+            a = i+a
+            lst.append(a)
+        return(lst)    
--- python basics/calculator.py
+++ python basics/calculator.py
@@ -1,2 +1,18 @@
-num1 = int(input("enter a number : "))
-num2 = int(input("enter a number : "))
+def Sum(n):
+    odd_sum = 0
+    even_sum =  0
+    
+    for i in range(len(n)):
+        if i%2 ==0:
+            even_sum += int(n[i])
+        else:
+            odd_sum += int(n[i])
+    ans = odd_sum + even_sum
+    return ans
+
+if __name__ == "__main__":
+    t = int(input())
+    for j in range(0,t):
+        n = [int(x) for x in input()]
+        
+        print(Sum(n))
